a70670e67190097c80c7be0a75745e3d5666cbe6
Johan Westling <johan@westling.ax>
2022-04-08 09:25:03 +0300

    Projects from json file

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,2 +1,50 @@
-console.log('hej');
+init();
 
+function init(){
+  loadEntries('./assets/json/projects.json');
+
+  // addEventListener('DOMContentLoaded', ...);
+}
+
+async function loadEntries(filepath){
+  const request = await fetch(filepath);
+  const response = await request.json();
+  const fragment = new DocumentFragment();
+  const container = document.querySelector('.portfolio-projects-items');
+
+  response.projects.forEach(item => {
+    const article = document.createElement('article');
+
+    article.classList.add('portfolio-card');
+    article.classList.add('portfolio-projects-item');
+    article.innerHTML = projectTemplate(item);
+
+    fragment.appendChild(article);
+  });
+
+  container.appendChild(fragment);
+}
+
+function projectTemplate({ title, image, description, techstack }){
+  return `
+    <header>
+      <h3>${title}</h3>
+    </header>
+
+    <figure>
+      <img src="${image.src}" alt="${image.alt}">
+      <figcaption>${image.caption}</figcaption>
+    </figure>
+
+    <p>${description}</p>
+
+    <footer>
+      <p>
+        Techstack:
+        <ul>
+          ${techstack.map(item => `<li>${item}</li>`).join('')}
+        </ul>
+      </p>
+    </footer>
+  `;
+}
